35a4e74d05f75571ea5039df8123395c98470105
BarbaraMaes <maesbarbara92@gmail.com>
2020-01-21 14:18:02 +0100

    added star clicking

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,37 @@
 $("#submit_add").submit(() => {
   $("#addModal").modal("hide");
 });
+
+function fill(el) {
+  let id = $(el).attr("id");
+  if (id > 1) {
+    console.log("id bigger than one");
+    $(el)
+      .prevAll()
+      .each(function() {
+        let newID = $(this).attr("id");
+        $(this).replaceWith(`<span id='${newID}' class='fas fa-star'></span>`);
+      });
+  }
+  $(el).replaceWith(`<span id='${id}' class='fas fa-star'></span>`);
+}
+
+function empty(el) {
+  let id = $(el).attr("id");
+  if (id < 5) {
+    $(el)
+      .nextAll()
+      .each(function() {
+        let newID = $(this).attr("id");
+        $(this).replaceWith(`<span id='${newID}' class='far fa-star'></span>`);
+      });
+  }
+}
+
+$("#stars").on("click", ".fa-star", function starClick() {
+  if ($(this).hasClass("far")) {
+    fill($(this));
+  } else if ($(this).hasClass("fas")) {
+    empty($(this));
+  }
+});
