4b3c35b36593a3cf1bfb384eecdfaba906276634
BarbaraMaes <maesbarbara92@gmail.com>
2020-01-22 13:09:28 +0100

    db

--- util/database.js
+++ util/database.js
@@ -2,27 +2,27 @@ const Sequelize = require("sequelize");
 
 //module.exports =
 let sequelize;
-if (process.env.HEROKU_POSTGRESQL_NAVY_URL) {
+/*if (process.env.HEROKU_POSTGRESQL_NAVY_URL) {
   sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_NAVY_URL, {
     dialect: "postgres",
     protocol: "postgres",
-    port: match[4],
-    host: match[3],
+    //port: match[4],
+    //host: match[3],
     logging: true //false
   });
-} else {
-  sequelize = new Sequelize("yelp", "postgres", "1234", {
-    host: "localhost",
-    dialect: "postgres",
-    operatorsAliases: false,
+} else {*/
+sequelize = new Sequelize("yelp", "postgres", "1234", {
+  host: "localhost",
+  dialect: "postgres",
+  operatorsAliases: false,
 
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  });
-}
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+//}
 
 module.exports = sequelize;
