690ff1bb2b83ad0ee0e98d61be7a6336d0916a9e
BarbaraMaes <maesbarbara92@gmail.com>
2020-01-23 22:28:46 +0100

    adjusted db

--- util/database.js
+++ util/database.js
@@ -3,13 +3,28 @@ const pg = require("pg");
 let sequelize;
 
 /*if ((process.env.NODE_ENV = "production")) {
-  sequelize = new Sequelize({
-    url: process.env.DEV_DATABASE_URL,
-    dialect: "postgres"
-  });
-} else {*/
+sequelize = new Sequelize(
+  `d6brb84vpoto1j`,
+  `bvwjdrxqrrslpg`,
+  `d8451bfc9e65fa7f3640a197b4a8145ba8beebb5b2f3cb707912d9dd19b4217a`,
+  {
+    host: `ec2-54-247-188-107.eu-west-1.compute.amazonaws.com`,
+    dialect: "postgres",
+    operatorsAliases: false,
+    uri:
+      "postgres://bvwjdrxqrrslpg:d8451bfc9e65fa7f3640a197b4a8145ba8beebb5b2f3cb707912d9dd19b4217a@ec2-54-247-188-107.eu-west-1.compute.amazonaws.com:5432/d6brb84vpoto1j",
+    port: "5432",
 
-/*sequelize = new Sequelize("yelp", "postgres", "1234", {
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+} else {
+  sequelize = new Sequelize("yelp", "postgres", "1234", {
   host: "localhost",
   dialect: "postgres",
   operatorsAliases: false,
@@ -22,6 +37,7 @@ let sequelize;
   }
 });
   */
+
 pg.defaults.ssl = true;
 sequelize = new Sequelize(
   `d6brb84vpoto1j`,
