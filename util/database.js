045f7eb48c93871b33cfb9f81096d8a3259f0d60
BarbaraMaes <maesbarbara92@gmail.com>
2020-01-23 22:12:12 +0100

    db

--- util/database.js
+++ util/database.js
@@ -8,6 +8,20 @@ let sequelize;
     dialect: "postgres"
   });
 } else {*/
+
+/*sequelize = new Sequelize("yelp", "postgres", "1234", {
+  host: "localhost",
+  dialect: "postgres",
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+  */
 pg.defaults.ssl = true;
 sequelize = new Sequelize(
   `d6brb84vpoto1j`,
