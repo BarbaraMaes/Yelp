bee79a9704489dac07feef781aac42744f2e550f
BarbaraMaes <maesbarbara92@gmail.com>
2020-01-23 22:09:37 +0100

    db adjust

--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const pg = require("pg");
 let sequelize;
 
 /*if ((process.env.NODE_ENV = "production")) {
@@ -7,18 +8,27 @@ let sequelize;
     dialect: "postgres"
   });
 } else {*/
-sequelize = new Sequelize(`yelp`, `postgres`, `1234`, {
-  host: `localhost`,
-  dialect: "postgres",
-  operatorsAliases: false,
+pg.defaults.ssl = true;
+sequelize = new Sequelize(
+  `d6brb84vpoto1j`,
+  `bvwjdrxqrrslpg`,
+  `d8451bfc9e65fa7f3640a197b4a8145ba8beebb5b2f3cb707912d9dd19b4217a`,
+  {
+    host: `ec2-54-247-188-107.eu-west-1.compute.amazonaws.com`,
+    dialect: "postgres",
+    operatorsAliases: false,
+    uri:
+      "postgres://bvwjdrxqrrslpg:d8451bfc9e65fa7f3640a197b4a8145ba8beebb5b2f3cb707912d9dd19b4217a@ec2-54-247-188-107.eu-west-1.compute.amazonaws.com:5432/d6brb84vpoto1j",
+    port: "5432",
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
-});
+);
 //}
 
 module.exports = sequelize;
